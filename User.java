621f268a485d0672ef515053b03961fa3cbe0cbe
suman <sumanbhamu@rocketmail.com>
2016-11-18 02:29:36 -0800

    Add files via upload

---
+++ User.java
@@ -0,0 +1,87 @@
+package com.suman.ecom.model;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table
+@Component
+
+public class User  {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+
+	private int user_id;
+	private String username;
+	private String password;
+	@Transient
+	private String confirmpassword;
+	private String emailid;
+	private String phno;
+	private String enabled;
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmpassword() {
+		return confirmpassword;
+	}
+
+	public void setConfirmpassword(String confirmpassword) {
+		this.confirmpassword = confirmpassword;
+	}
+
+	public String getEmailid() {
+		return emailid;
+	}
+
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+
+	public String getPhno() {
+		return phno;
+	}
+
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+
+	public String getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(String enabled) {
+		this.enabled = enabled;
+	}
+
+}
